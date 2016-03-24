bebe201244131097f53b2d4d8a5ff5313faeadac
Ainur <ainurmullin@mail.ru>
2016-03-24 10:48:19 +0300

    change path in style.css

--- mail/src/test/java/TestMailing.java
+++ mail/src/test/java/TestMailing.java
@@ -1,19 +1,17 @@
-package com.shop.itis.Utils;
-
 import com.shop.itis.MailService;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestMailing {
-    public static void main(String[] args) {
 
-        ApplicationContext context = new FileSystemXmlApplicationContext("WEB-INF/spring/ApplicationContext.xml");
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("mail.xml");
 
         //Get the mailer instance
         MailService mailer = (MailService) context.getBean("mailService");
 
         //Send a composed mail
         mailer.sendMail("ainurmullin@mail.ru", "Test Subject", "Testing body", "ainur6969@gmail.com");
-
     }
+
 }
